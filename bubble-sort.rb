2dd9d07668419d2eccb68ac5fb90bbfb7303045d
IslesPJ <isles.pj@gmail.com>
2026-04-28 20:43:07 +0900

    Added bubble sort ruby file.

---
+++ bubble-sort.rb
@@ -0,0 +1,21 @@
+def bubble_sort(array)
+    index = 0
+    value_holder = 0
+    swapped = true
+    until (swapped == false)
+        swapped = false
+        while (index < array.length-1)
+            if (array[index] > array[index+1])
+                value_holder = array[index+1]
+                array[index+1] = array[index]
+                array[index] = value_holder
+                swapped = true
+            end
+        index += 1
+        end
+        index = 0
+    end
+    array
+end
+
+p bubble_sort [24, 7, 43, 12, 38, 2, 49, 15, 33, 27, 5, 41, 19, 46, 30, 8, 35, 11, 48, 23, 1, 37, 14, 50, 26, 4, 32, 21, 44, 9, 36, 18, 45, 10, 29, 3, 25, 40, 13, 31, 22, 6, 39, 16, 47, 28, 17, 34, 20, 42] 
